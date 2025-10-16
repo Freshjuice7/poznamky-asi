8df81bc5b1dd1b958b499a5e7cf1c07b86721d7d
Freshjuice7 <vasvac21@gasos-ro.cz>
2025-10-16 15:10:07 +0200

    uml

---
+++ src/uml.java
@@ -0,0 +1,35 @@
+/*Letenka
+-------------------
+ - cisloLetenky: String
+- odletoveMisto: String
+- ciloveMisto: String
+- datumOdletu: String
+-------------------
+        + get/set
++ toString()
+
+DomaciLetenka
+-------------------
+        - cisloTerminalu: int
+-------------------
+        + get/set
++ toString()
+
+MezinarodniLetenka
+-------------------
+        - zemePristani: String
+-------------------
+        + get/set
++ toString()
+
+SpravceLetenek
+-------------------
+        - list: ArrayList<Letenka>
+-------------------
+        + pridatLetenku()
++ odebratLetenku()
++ vypsatLetenky()
++ najitLetenkuPodleCisla()
++ smazatLetenkyPoOdletu()
++ vypsatLetenkyZeZeme()
+*/
